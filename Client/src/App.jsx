0d969b50d0776a627a61c93a45c17bb85d7e2db7
milagros <miliguzman0@gmail.com>
2023-09-21 00:10:47 -0300

    addedbasic Front

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './views/home/home';
+import Error from './views/error/Error';
+import About from './views/about/About';
+import Detail from './views/details/Detail';
+import NavBar from './components/NavBar/NavBar';
+import Form from './components/Form/Form';
+import Favorites from './views/Favorites/favorites';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFavs } from './redux/actions/favoritesActions';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+import { getCharacter, addRandom, deleteCharacter } from './redux/actions/characterActions';
+import { setUser } from './redux/actions/userActions';
 
 function App() {
-  const [count, setCount] = useState(0)
+   const {allCharacters} = useSelector(state=>state.characters);
+   const [access, setAccess] = useState(false);
+   const dispatch = useDispatch();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   
+   const navigate = useNavigate();
+   let location = useLocation();
+
+   async function searchHandler(id) {
+      dispatch(getCharacter(id));
+   }
+
+   async function addingRandom(){
+      let loTengo =[];
+      let random= Math.floor(Math.random()*826);
+
+      try {
+         if(!loTengo.includes(random)){
+            loTengo.push(random);
+            const {data} = await axios(`http://localhost:3001/rickandmorty/character/${random}`);
+            if (data.name) {
+               if(!allCharacters.includes(data))
+                 dispatch(addRandom(data));
+               } else {
+               window.alert('¡No hay personajes con este ID!');
+            }
+         }
+      } catch (error) {
+         throw new Error(error.message);
+      }
+   }
+   function closeCard (id){
+      dispatch(deleteCharacter(id))
+   }
+   async function login(userData) {
+      try {
+         const { email, password } = userData;
+         const URL = 'http://localhost:3001/rickandmorty/login';
+
+         const {data} = await axios.post(URL,{
+            email,
+            password,
+         });
+         if (data) {
+         dispatch(setUser(data.id));
+         dispatch(setFavs(data.favorites));
+         setAccess(data);
+         }
+         
+      } catch (error) {
+         console.error(error.message);
+      }
+   }
+   
+
+   
+   useEffect(() => {
+      !access && navigate('/');
+      access && navigate('/home');
+   }, [access]);
+
+   return (
+     <div>
+         { ( location.pathname !== '/' && location.pathname !== '*' ) && <NavBar addingRandom={addingRandom} onSearch={searchHandler}></NavBar>}
+         
+         <Routes>
+            <Route path='/favorites' element={<Favorites/>} />
+            <Route path='/' element= {<Form login={login} />}/>
+            <Route path='/home' element= {<Home onClose={closeCard}/>}/>
+            <Route path='/detail/:id' element= {<Detail/>}/>
+            <Route path='/about' element= {<About/>}/>
+            <Route path='*' element= {<Error/>}/>
+         </Routes>
+     </div>
+   )
 }
 
-export default App
+export default App;
